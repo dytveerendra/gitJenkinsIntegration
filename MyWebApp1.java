7785cf39b34bea47e3ef0c6008dc4e5620f18108
veerendra kumar <qa.veerendra@gmail.com>
2020-05-20 15:12:02 +0530

    updated How are you msg and alignment

--- MyWebApp1.java
+++ MyWebApp1.java
@@ -1,10 +1,6 @@
 public class MyWebApp1{
-
-public static void main(String[] args){
-
-System.out.println("Hel1lo Veerendra kumar!");
-  System.out.println("How are you?");
-
-}
-
+  public static void main(String[] args){
+    System.out.println("Hel1lo Veerendra kumar!");
+    System.out.println("How are you doing?");
+  }
 }
